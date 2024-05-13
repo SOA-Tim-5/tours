dd08db339d5fc499b875a0d92d5526cfdd496d9b
Ivana Kovacevic <kovacevic.ra15.2020@uns.ac.rs>
2024-05-14 00:38:14 +0200

    feat: get all facility requests grpc

--- Tours/WebServerWithDB/proto/publicFacilityRequest/publicFacilityRequest_grpc.pb.go
+++ Tours/WebServerWithDB/proto/publicFacilityRequest/publicFacilityRequest_grpc.pb.go
@@ -11,6 +11,7 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -23,6 +24,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type PublicFacilityRequestServiceClient interface {
 	Create(ctx context.Context, in *PublicFacilityRequestCreateDto, opts ...grpc.CallOption) (*PublicFacilityRequestResponseDto, error)
+	GetAllFacilityRequest(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*PublicFacilityRequestListResponse, error)
 }
 
 type publicFacilityRequestServiceClient struct {
@@ -42,11 +44,21 @@ func (c *publicFacilityRequestServiceClient) Create(ctx context.Context, in *Pub
 	return out, nil
 }
 
+func (c *publicFacilityRequestServiceClient) GetAllFacilityRequest(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*PublicFacilityRequestListResponse, error) {
+	out := new(PublicFacilityRequestListResponse)
+	err := c.cc.Invoke(ctx, "/PublicFacilityRequestService/GetAllFacilityRequest", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // PublicFacilityRequestServiceServer is the server API for PublicFacilityRequestService service.
 // All implementations must embed UnimplementedPublicFacilityRequestServiceServer
 // for forward compatibility
 type PublicFacilityRequestServiceServer interface {
 	Create(context.Context, *PublicFacilityRequestCreateDto) (*PublicFacilityRequestResponseDto, error)
+	GetAllFacilityRequest(context.Context, *emptypb.Empty) (*PublicFacilityRequestListResponse, error)
 	mustEmbedUnimplementedPublicFacilityRequestServiceServer()
 }
 
@@ -56,6 +68,9 @@ type UnimplementedPublicFacilityRequestServiceServer struct {
 
 func (UnimplementedPublicFacilityRequestServiceServer) Create(context.Context, *PublicFacilityRequestCreateDto) (*PublicFacilityRequestResponseDto, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
+}
+func (UnimplementedPublicFacilityRequestServiceServer) GetAllFacilityRequest(context.Context, *emptypb.Empty) (*PublicFacilityRequestListResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetAllFacilityRequest not implemented")
 }
 func (UnimplementedPublicFacilityRequestServiceServer) mustEmbedUnimplementedPublicFacilityRequestServiceServer() {
 }
@@ -89,6 +104,24 @@ func _PublicFacilityRequestService_Create_Handler(srv interface{}, ctx context.C
 	return interceptor(ctx, in, info, handler)
 }
 
+func _PublicFacilityRequestService_GetAllFacilityRequest_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(emptypb.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PublicFacilityRequestServiceServer).GetAllFacilityRequest(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/PublicFacilityRequestService/GetAllFacilityRequest",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PublicFacilityRequestServiceServer).GetAllFacilityRequest(ctx, req.(*emptypb.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // PublicFacilityRequestService_ServiceDesc is the grpc.ServiceDesc for PublicFacilityRequestService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -99,6 +132,10 @@ var PublicFacilityRequestService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Create",
 			Handler:    _PublicFacilityRequestService_Create_Handler,
+		},
+		{
+			MethodName: "GetAllFacilityRequest",
+			Handler:    _PublicFacilityRequestService_GetAllFacilityRequest_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
